7fda0d2f2643dd391f192043dc2d92d9d9d32d34
zeoprogram <alzi014299@gmail.com>
2026-09-09 17:15:51 +0700

    Create App.tsx

---
+++ frontend/src/pages/frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "@/pages/Home";
+import Admin from "@/pages/Admin";
+
+
+export default function App() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+      <Route
+        path="/admin"
+        element={<Admin />}
+      />
+    </Routes>
+  );
+}
