d420afc48ef8a79f750b21da54bc7a3578b5699b
zeoprogram <alzi014299@gmail.com>
2026-09-09 17:06:52 +0700

    Create seed.py

---
+++ backend/backend/seed.py
@@ -0,0 +1,32 @@
+HG-2026-DEMO01
+Raka Pratama
+Motor
+Yamaha NMAX 2023
+Paint Correction 2-Step
+Ceramic Coating 9H+
+Workshop
+2026-08-20
+09:00 - 11:00
+in_progress
+Rp1.325.000
+HG-2026-DEMO02
+Maya Sari
+Mobil
+Toyota Fortuner
+Wet Gloss Finish
+Engine Deep Degreaser
+Home Service
+2026-08-20
+13:00 - 15:00
+confirmed
+Rp775.000
+  HG-2026-DEMO03
+Dimas Wicaksono
+Helm
+Shoei X-Spirit
+Helmet Spa
+Workshop
+2026-08-19
+15:00 - 16:00
+completed
+Rp150.000
